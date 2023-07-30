6b2a13520873fc6fffaa5321b581658c29def39b
Vinícius Frigulha Ribeiro <100040439+vinifrigulha@users.noreply.github.com>
2023-07-30 20:27:52 -0300

    Create Ex_01.c

---
+++ Estrutura_de_Dados_I/Atv_Diagnostica/Ex_01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int vetor[10];
+  int soma = 0;
+
+  printf("Digite 10 números e obtenha a soma deles.\n\n");
+
+  for(int i = 0; i < 10; i++){
+    printf("%dº valor: ", i+1);
+    scanf("%d", &vetor[i]);
+    soma += vetor[i];
+  }
+  
+  printf("\nA soma dos 10 números é igual a %d", soma);
+
+  return 0;
+}
