46883b61cc712d5b543468e96c3d12e2e69ef6a6
Vinícius Frigulha Ribeiro <100040439+vinifrigulha@users.noreply.github.com>
2023-07-30 20:51:07 -0300

    Update Ex_02.c

--- Estrutura_de_Dados_I/Atv_Diagnostica/Ex_02.c
+++ Estrutura_de_Dados_I/Atv_Diagnostica/Ex_02.c
@@ -3,7 +3,7 @@
 int main(){
   int vetor[10];
 
-  printf("Digite 10 números em qualquer ordem. Ao final, direi se eles estão na ordem crescente ou não.\n\n");
+  printf("Digite 10 números quaisquer.\n\n");
 
   for(int i = 0; i < 10; i++){
     printf("%dº número: ", i+1);
@@ -12,11 +12,11 @@ int main(){
   
   for(int i = 0; i < 9; i++){
     if(vetor[i+1] - vetor[i] <= 0){
-      printf("A sua ordem não estava em ordem crescente");
+      printf("A ordem digitada não estava em ordem crescente");
       break;
     }
     else if(i+1 == 9 && vetor[i+1] - vetor[i] > 0){
-      printf("A sua ordem estava em ordem crescente.");
+      printf("A ordem digitada estava em ordem crescente.");
     }
   }
 
