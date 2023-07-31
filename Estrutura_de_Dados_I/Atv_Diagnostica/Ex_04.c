2b73846ceed91bfef806635453e4505ceb049e06
Vinícius Frigulha Ribeiro <100040439+vinifrigulha@users.noreply.github.com>
2023-07-30 21:12:22 -0300

    Create Ex_04.c

---
+++ Estrutura_de_Dados_I/Atv_Diagnostica/Ex_04.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void preencherVetor(int vetor[]){
+  for(int i = 0; i < 10; i++){
+    printf("%dº número: ", i+1);
+    scanf("%d", &vetor[i]);
+  }
+}
+
+int main(){
+  int vetorA[10];
+  int vetorB[10];
+  int vetorC[10];
+
+  printf("Digite 10 números quaiquer para o vetor A.\n\n");
+  preencherVetor(vetorA);
+
+  printf("\nDigite 10 números quaiquer para o vetor B.\n\n");
+  preencherVetor(vetorB);
+
+  for(int i = 0; i < 10; i++){
+    vetorC[i] = vetorA[i] + vetorB[i];
+  }
+
+  printf("\nO vetor C tem os seguintes valores:\n\n [");
+  for(int i = 0; i < 10; i++){
+    if(i < 9) printf("%d, ", vetorC[i]);
+    else printf("%d]", vetorC[i]);
+  }
+  
+  return 0;
+}
