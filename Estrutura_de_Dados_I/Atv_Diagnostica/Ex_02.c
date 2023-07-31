bf515f120c5021e7732c683033a08b9cbc3d6547
Vinícius Frigulha Ribeiro <100040439+vinifrigulha@users.noreply.github.com>
2023-07-30 23:16:06 -0300

    Update Ex_02.c

--- Estrutura_de_Dados_I/Atv_Diagnostica/Ex_02.c
+++ Estrutura_de_Dados_I/Atv_Diagnostica/Ex_02.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 
-int main(){
-  int vetor[10];
+int main() {
+    int vetor[10];
 
-  printf("Digite 10 números quaisquer.\n\n");
-
-  for(int i = 0; i < 10; i++){
-    printf("%dº número: ", i+1);
-    scanf("%d", &vetor[i]);
-  }
-  
-  for(int i = 0; i < 9; i++){
-    if(vetor[i+1] - vetor[i] <= 0){
-      printf("A ordem digitada não estava em ordem crescente");
-      break;
+    printf("Digite 10 números quaisquer.\n\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%dº número: ", i + 1);
+        scanf("%d", &vetor[i]);
     }
-    else if(i+1 == 9 && vetor[i+1] - vetor[i] > 0){
-      printf("A ordem digitada estava em ordem crescente.");
-    }
-  }
 
-  return 0;
+    int ordem_crescente = 1;
+    for (int i = 0; i < 9; i++) {
+        if (vetor[i + 1] <= vetor[i]) {
+            ordem_crescente = 0; // '0' em C significa False - qualquer coisa diferente de '0' é True
+            break;
+        }
+    }
+
+    if (ordem_crescente) {
+        printf("A ordem digitada estava em ordem crescente.\n");
+    } else {
+        printf("A ordem digitada não estava em ordem crescente.\n");
+    }
+
+    return 0;
 }
