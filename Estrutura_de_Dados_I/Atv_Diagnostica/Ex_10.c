d8d67d7f06bc7b1c2dcdc118354f08b8d8f717d7
Vinícius Frigulha Ribeiro <100040439+vinifrigulha@users.noreply.github.com>
2023-07-30 22:01:41 -0300

    Create Ex_10.c

---
+++ Estrutura_de_Dados_I/Atv_Diagnostica/Ex_10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+  int vetor[10];
+  int soma = 0;
+  double media;
+
+  printf("Digite 10 números e obtenha a soma deles.\n\n");
+  for(int i = 0; i < 10; i++){
+    printf("%dº valor: ", i+1);
+    scanf("%d", &vetor[i]);
+    soma += vetor[i];
+  }
+
+  media = soma / 10.0;
+  
+  printf("\nA média aritmética dos valores fornecidos é igual a %.2f", media);
+
+  return 0;
+}
