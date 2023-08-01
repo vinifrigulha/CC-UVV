ffa094ad78075c97b813e4169000abdd8a5ffb91
Vinícius Frigulha Ribeiro <100040439+vinifrigulha@users.noreply.github.com>
2023-08-01 09:29:54 -0300

    Update structPonto.c

--- Estrutura_de_Dados_I/Atv_Diagnostica/structPonto.c
+++ Estrutura_de_Dados_I/Atv_Diagnostica/structPonto.c
@@ -1,20 +1,23 @@
 #include <math.h>
 #include <stdio.h>
 
+/* Definindo a estrutura de um ponto */
 struct Ponto {
   float x;
   float y;
 };
 
+/* Função para setar o ponto */
 struct Ponto setPonto(struct Ponto p){
   printf("\nX: ");
-  scanf("%f", &p.x); // Salvando a coordenada x do ponto A
+  scanf("%f", &p.x); // Salvando a coordenada x do ponto
   printf("Y: ");
-  scanf("%f", &p.y); // Salvando a coordenada y do ponto A
+  scanf("%f", &p.y); // Salvando a coordenada y do ponto
 
   return p;
 }
 
+/* Função para calcular a distância entre dois pontos */
 double calculaDistancia(struct Ponto a, struct Ponto b) {
   float subX = pow((a.x - b.x), 2); // (x' - x")^2
   float subY = pow((a.y - b.y), 2); // (y' - y")^2
@@ -29,8 +32,7 @@ int main() {
   printf("\nDigite as coordenadas do ponto B:");
   struct Ponto b = setPonto(b);
 
-  printf("\nA distância entre A(%.2f, %.2f) e B (%.2f, %.2f) é %.2f", a.x, a.y,
-         b.x, b.y, calculaDistancia(a, b));
+  printf("\nA distância entre A(%.2f, %.2f) e B (%.2f, %.2f) é %.2f", a.x, a.y, b.x, b.y, calculaDistancia(a, b));
 
   return 0;
 }
