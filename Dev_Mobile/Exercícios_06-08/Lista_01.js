4094370dd576c24279a4357d54bb3d4c362049be
Vinícius Frigulha Ribeiro <100040439+vinifrigulha@users.noreply.github.com>
2023-08-04 14:17:57 -0300

    Update Lista_01.js

--- Dev_Mobile/Exercícios_06-08/Lista_01.js
+++ Dev_Mobile/Exercícios_06-08/Lista_01.js
@@ -1,35 +1,55 @@
 // Questão 01
-/** 
-* Esta função cria um vetor 10 objetos por padrao
-* ou um vetor com uma quantidade definida pelo usuário de objetos
-* com os atributos nome : String e idade : Number
-*
-*
-* @param {number} n - o número de objetos dentro do vetor
-* @returns {object} - o vetor com os objetos informados
-*/
-function geraVetor(n = 10){
+function geraVetor() {
+  /* Criando um vetor vazio */
   let vetor = [];
-  
-  for (i = 0; i < n; i++){
-    console.log(`Objeto ${i+1}:`);
-    let name = prompt('Nome: ');
-    let age = prompt('Idade: ');
 
-    let atributos = {
-      nome: name,
-      idade: age
-    }
+  /* Criando 10 objetos */
+  let obj1 = { nome: "Carlos", idade: 43 };
+  let obj2 = { nome: "Amanda", idade: 28 };
+  let obj3 = { nome: "Rodrigo", idade: 62 };
+  let obj4 = { nome: "Bianca", idade: 19 };
+  let obj5 = { nome: "Fernando", idade: 35 };
+  let obj6 = { nome: "Camila", idade: 62 };
+  let obj7 = { nome: "Gustavo", idade: 25 };
+  let obj8 = { nome: "Letícia", idade: 30 };
+  let obj9 = { nome: "Paulo", idade: 48 };
+  let obj10 = { nome: "Mariana", idade: 25 };
 
-    vetor.push(atributos)
-    console.log('\n');
-  };
+  /* Inserindo os objetos no vetor */
+  vetor.push(obj1);
+  vetor.push(obj2);
+  vetor.push(obj3);
+  vetor.push(obj4);
+  vetor.push(obj5);
+  vetor.push(obj6);
+  vetor.push(obj7);
+  vetor.push(obj8);
+  vetor.push(obj9);
+  vetor.push(obj10);
 
   return vetor;
 };
 
+// Gerando um vetor com 10 objetos
+let meuVetor = geraVetor();
+console.log('Vetor original:');
+console.log(meuVetor);
 
-let start = prompt('Digite um número maior que zero ou aperte "Enter": ')
-let vetor = (start > 0 ? geraVetor(start) : (start == "" ? geraVetor() : 'Vetor não criado.'))
+// Questão 02
+// a) Ordenando o vetor pelo nome
+let ordVetorNome = (meuVetor) => {
+  return meuVetor.sort((a, b) => {
+    return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0
+  })
+};
+console.log('\nVetor ordenado pelo nome:');
+console.log(ordVetorNome(meuVetor));
 
-console.log(vetor);
+// b) Ordenando o vetor por idade
+let ordVetorIdade = (meuVetor) => {
+  return meuVetor.sort((a, b) => {
+    return a.idade < b.idade ? -1 : a.idade > b.idade ? 1 : 0
+  })
+};
+console.log('\nVetor ordenado pela idade:');
+console.log(ordVetorIdade(meuVetor));
