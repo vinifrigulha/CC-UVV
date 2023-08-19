841c3ef370a0f828c88e8b45ef9869d3476fa4d0
Vinícius Frigulha Ribeiro <100040439+vinifrigulha@users.noreply.github.com>
2023-08-18 22:31:16 -0300

    Create Primos.c

---
+++ Estrutura_de_Dados_I/Ponteiro/Primos.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void parImpar(int v[], int size) {
+  // Loop do array
+  for (int i = 0; i < size; i++) {
+    // Loop divisores
+    if (v[i] == 2) {
+      printf("\n%d é primo", v[i]);
+    } else {
+      for (int j = 2; j < v[i]; j++) {
+        // Tenta achar divisores
+        if (v[i] % j == 0) {
+          printf("\n%d não é primo!", v[i]);
+          break;
+        } else {
+          printf("\n%d é primo", v[i]);
+          break;
+        }
+      }
+    }
+  }
+}
+
+int main() {
+  int tamanho = 5;
+  int num[] = {12, 2, 3, 5, 87};
+
+  parImpar(num, tamanho);
+
+  return 0;
+}
